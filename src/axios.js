f0cf44a4829e8e7c41ce20cf59328dda1559e1fe
“Elsa-zlt” <2903250242@qq.com>
2022-05-06 16:00:18 +0800

    change

--- src/axios.js
+++ src/axios.js
@@ -32,12 +32,10 @@ axios.interceptors.response.use(response => {
     if(error.response.data) {
       error.message = error.response.data.msg
     }
-
     if(error.response.status === 401) {
       store.commit("REMOVE_INFO")
       router.push("/login")
     }
-
     Element.Message.error(error.message, {duration: 3 * 1000})
     return Promise.reject(error)
   }
